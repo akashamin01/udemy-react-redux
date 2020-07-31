59e2feda39fe44398c41c567f8658975807237b0
Akash Amin <38359324+akashamin01@users.noreply.github.com>
2020-07-31 13:53:59 +0530

    Update index.js

--- weatherapp/src/index.js
+++ weatherapp/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReduxPromise from 'redux-promise';
 import {Provider} from 'react-redux';
 import {createStore,  applyMiddleware} from 'redux';
-import ReduxPromise from 'redux-promise';
+
 import App from './components/App';
 
 
